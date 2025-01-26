24e731cd44c6c540b6197292bdfb1443f6b324e1
João Campos <joaon.c.jv@gmail.com>
2025-01-26 18:24:45 -0300

    ajustando versão mobile

--- js/projects-splide.js
+++ js/projects-splide.js
@@ -1,9 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
-new Splide('.splide.projects-container', {
+new Splide('.splide.projects-the-bests', {
     type: 'slide',
     perPage: 5,
     pagination: false,
-    focus: 'center',
+    gap: '10px',
+    arrows: true,
+    breakpoints: {
+        1400: {
+            perPage: 4,
+         },
+        1200: {
+            perPage: 3,
+        },
+        768: { perPage: 2 },
+        468: { perPage: 1 },
+    },
+    easing: 'ease',
+    speed: 1500,
+    
+}).mount();
+x.style.display = "inherit";
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+new Splide('.splide.projects-frontend', {
+    type: 'slide',
+    perPage: 5,
+    pagination: false,
     gap: '20px',
     arrows: true,
     breakpoints: {
@@ -24,37 +47,11 @@ x.style.display = "inherit";
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-new Splide('.splide.projects-container2', {
+new Splide('.splide.projects-fullstack', {
     type: 'slide',
     perPage: 5,
     pagination: false,
-    focus: 'center',
-    gap: '20px',
-    arrows: true,
-    breakpoints: {
-        1400: {
-            perPage: 4,
-         },
-        1200: {
-            perPage: 3,
-        },
-        768: { perPage: 2 },
-        468: { perPage: 1 },
-    },
-    easing: 'ease',
-    speed: 1500,
-    
-}).mount();
-x.style.display = "inherit";
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-new Splide('.splide.projects-container3', {
-    type: 'slide',
-    perPage: 5,
-    pagination: false,
-    focus: 'center',
-    gap: '20px',
+    gap: '10px',
     arrows: true,
     breakpoints: {
         1400: {
@@ -75,12 +72,11 @@ x.style.display = "inherit";
 
 
 document.addEventListener('DOMContentLoaded', function () {
-new Splide('.splide.projects-container4', {
+new Splide('.splide.projects-backend', {
     type: 'slide',
     perPage: 5,
     pagination: false,
-    focus: 'center',
-    gap: '20px',
+    gap: '10px',
     arrows: true,
     breakpoints: {
         1400: {
