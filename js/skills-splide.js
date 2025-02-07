d88319e95e082c9d0a64f344cd18af2cab71f6cc
João Campos <joaon.c.jv@gmail.com>
2025-02-07 20:14:58 -0300

    fazendo a pagina de habilidades

---
+++ js/skills-splide.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+new Splide('.splide.skills-splide', {
+    type: 'slide',
+    perPage: 1,
+    pagination: true,
+    gap: '20px',
+    arrows: true,
+    easing: 'ease',
+    speed: 1500,
+    
+}).mount();
+x.style.display = "inherit";
+});
