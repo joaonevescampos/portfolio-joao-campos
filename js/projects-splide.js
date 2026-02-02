fa2c5718b984fac596d45131a7b0e492befa0385
João Campos <joaon.c.jv@gmail.com>
2026-02-02 13:51:18 -0300

    feat: upgrade portfolio

--- js/projects-splide.js
+++ js/projects-splide.js
@@ -1,97 +1,91 @@
-document.addEventListener('DOMContentLoaded', function () {
-new Splide('.splide.projects-the-bests', {
-    type: 'slide',
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide.projects-the-bests", {
+    type: "slide",
     perPage: 5,
     perMove: 1,
     pagination: false,
-    gap: '10px',
     arrows: true,
     breakpoints: {
-        2200: {
-            perPage: 4,
-         },
-        1600: {
-            perPage: 3,
-        },
-        1200: { perPage: 2 },
-        768: { perPage: 1 },
+      2200: {
+        perPage: 4,
+      },
+      1600: {
+        perPage: 3,
+      },
+      1200: { perPage: 2 },
+      768: { perPage: 1 },
     },
-    easing: 'ease',
+    easing: "ease",
     speed: 500,
-    
-}).mount();
-x.style.display = "inherit";
+  }).mount();
+  x.style.display = "inherit";
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-new Splide('.splide.projects-frontend', {
-    type: 'slide',
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide.projects-frontend", {
+    type: "slide",
     perPage: 5,
+    perMove: 1,
     pagination: false,
-    gap: '20px',
     arrows: true,
-      breakpoints: {
-        2200: {
-            perPage: 4,
-         },
-        1600: {
-            perPage: 3,
-        },
-        1200: { perPage: 2 },
-        768: { perPage: 1 },
+    breakpoints: {
+      2200: {
+        perPage: 4,
+      },
+      1600: {
+        perPage: 3,
+      },
+      1200: { perPage: 2 },
+      768: { perPage: 1 },
     },
-    easing: 'ease',
+    easing: "ease",
     speed: 500,
-    
-}).mount();
-x.style.display = "inherit";
+  }).mount();
+  x.style.display = "inherit";
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-new Splide('.splide.projects-fullstack', {
-    type: 'slide',
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide.projects-fullstack", {
+    type: "slide",
     perPage: 5,
+    perMove: 1,
     pagination: false,
-    gap: '10px',
     arrows: true,
-       breakpoints: {
-        2200: {
-            perPage: 4,
-         },
-        1600: {
-            perPage: 3,
-        },
-        1200: { perPage: 2 },
-        768: { perPage: 1 },
+    breakpoints: {
+      2200: {
+        perPage: 4,
+      },
+      1600: {
+        perPage: 3,
+      },
+      1200: { perPage: 2 },
+      768: { perPage: 1 },
     },
-    easing: 'ease',
+    easing: "ease",
     speed: 500,
-    
-}).mount();
-x.style.display = "inherit";
+  }).mount();
+  x.style.display = "inherit";
 });
 
-
-document.addEventListener('DOMContentLoaded', function () {
-new Splide('.splide.projects-backend', {
-    type: 'slide',
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide.projects-backend", {
+    type: "slide",
     perPage: 5,
+    perMove: 1,
     pagination: false,
-    gap: '10px',
     arrows: true,
-       breakpoints: {
-        2200: {
-            perPage: 4,
-         },
-        1600: {
-            perPage: 3,
-        },
-        1200: { perPage: 2 },
-        768: { perPage: 1 },
+    breakpoints: {
+      2200: {
+        perPage: 4,
+      },
+      1600: {
+        perPage: 3,
+      },
+      1200: { perPage: 2 },
+      768: { perPage: 1 },
     },
-    easing: 'ease',
+    easing: "ease",
     speed: 500,
-    
-}).mount();
-x.style.display = "inherit";
+  }).mount();
+  x.style.display = "inherit";
 });
